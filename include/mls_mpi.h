70285199d66a59189ebbefd604d3b02fdfcb13bd
Marat_Khabibulin <marat.habibulin@nami.ru>
2022-03-10 15:37:37 +0300

    Перемещен файл с исходным кодом.

--- include/mls_mpi.h
+++ include/mls_mpi.h
@@ -1,10 +1,15 @@
 #ifndef MLS_MPI_H
 #define MLS_MPI_H
 
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
 class mls_mpi
 {
 public:
+  mls_mpi();
 private:
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
 };
 
 #endif // MLS_MPI_H
