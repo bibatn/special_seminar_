5ca16ee05b42e55fb97be8ba66da38c00421d609
Marat_Khabibulin <marat.habibulin@nami.ru>
2022-03-29 15:58:56 +0300

    Добавлен метод go_to_line.

--- include/mls_mpi.h
+++ include/mls_mpi.h
@@ -3,14 +3,17 @@
 
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#include <istream>
 
 class mls_mpi
 {
 public:
   mls_mpi();
   void read(std::string & path, pcl::PointCloud<pcl::PointXYZ>::Ptr & cloud);
+  std::istream& go_to_line(std::istream& file, unsigned int num);
 private:
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
 };
 
 #endif // MLS_MPI_H
+
--- src/mls_mpi.cpp
+++ src/mls_mpi.cpp
@@ -4,3 +4,11 @@ mls_mpi::mls_mpi():cloud (new pcl::PointCloud<pcl::PointXYZ> ())
 {
 
 }
+
+std::istream& mls_mpi::go_to_line(std::istream& file, unsigned int num){
+    file.seekg(std::ios::beg);
+    for(int i=0; i < num - 1; ++i){
+        file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    }
+    return file;
+}
