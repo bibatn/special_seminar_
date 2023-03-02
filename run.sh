b6065669a9cf76decbb9799e2d06fa64f9b2ae6b
bibatn <habibulinmarat@icloud.com>
2023-03-02 16:59:36 +0300

    Добавлен скрипт.

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-mpirun -np 1 cmake-build-debug/surface /home/m/projects/special_seminar/1.pcd /home/m/projects/special_seminar/2.pcd 0.09
+#mpirun -np 1 cmake-build-debug/surface /home/m/projects/special_seminar/1.pcd /home/m/projects/special_seminar/2.pcd 0.09
+mpirun -np 1 cmake-build-debug/surface /home/m/projects/course_work/LadaVesta_normalized.pcd /home/m/projects/special_seminar/LadaVesta_normalized.pcd 0.0008
