bcf8d279298fb80bd3a627dd2fba556eb5fda860
bibatn <habibulinmarat@icloud.com>
2023-03-03 17:18:12 +0300

    Изменен параметр.

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #mpirun -np 1 cmake-build-debug/surface /home/m/projects/special_seminar/1.pcd /home/m/projects/special_seminar/2.pcd 0.09
-mpirun -np 1 cmake-build-debug/surface /home/m/projects/course_work/LadaVesta_normalized.pcd /home/m/projects/special_seminar/LadaVesta_normalized.pcd 0.0008
+#mpirun -np 1 cmake-build-debug/surface /home/m/projects/course_work/LadaVesta_normalized.pcd /home/m/projects/special_seminar/LadaVesta_normalized.pcd 0.0008
+mpirun -np 1 cmake-build-debug/surface /home/m/projects/course_work/LadaVesta_normalized_split.pcd /home/m/projects/special_seminar/LadaVesta_splited.pcd 0.0008
