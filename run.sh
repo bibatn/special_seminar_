a55e1d9ea88eca762bc965861d9b8d8710c9e98e
bibatn <habibulinmarat@icloud.com>
2023-03-03 19:26:24 +0300

    Правка.

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #bsub -n 1 -W 00:10 -o result -e /dev/null OMP_NUM_THREADS=1 ./surface 1.pcd 0.09
-mpisubmit.pl -p 1 build/surface 1.pcd 0.09
+# http://hpc.cmc.msu.ru/node/243  
+mpisubmit.pl -p 1 --stdout result build/surface LadaVesta_normalized.pcd 0.0008
